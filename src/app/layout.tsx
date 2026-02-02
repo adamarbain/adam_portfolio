8828c6fa05ad7d4eab45594b216bd3715c92b219
adamarbain <u2101735@siswa.um.edu.my>
2026-02-02 23:36:15 +0800

    Revise resume content with updated educational details, skills, and project descriptions. Add new project images for "Drone4Dengue" and "Imaginur." Enhance homepage with an expanded "About Me" section and a project image carousel for better presentation.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
             <footer className="border-t py-6 md:py-0 bg-background/80">
               <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
                 <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                  © 2025 adamarbain
+                  © 2026 adamarbain
                 </p>
               </div>
             </footer>
